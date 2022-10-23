1e2d11e7f00e72540f8797cfcdfc2be6075b36b8
Mano S <manokcg@gmail.com>
2022-10-23 23:00:58 +0530

    food-token

---
+++ config/dbConfig.js
@@ -0,0 +1,6 @@
+const mongodb = require('mongodb')
+const dbName = 'food-token-generator'
+const dbUrl = `mongodb+srv://Mano9940:Dan09940@cluster0.eurlw.mongodb.net/${dbName}`
+
+
+module.exports = {mongodb,dbName,dbUrl}
